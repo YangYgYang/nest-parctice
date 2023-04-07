77d6171bdfbf407a8be824eb049bbedbfe214a1c
YangYgYang <you@example.com>
2023-04-07 11:18:51 +0800

    add:test http method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get ,Delete,Post,Put,Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get(':userId')
+  getUserDataById(@Param('userId')id): string {
+    return this.appService.getHello();
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Delete()
+  list():string{
+    return this.appService.deleteList();
+  }
+
+  @Post()
+  creatList():string{
+    return this.appService.createList();
+  }
+
+  @Put()
+  updateList():string{
+    return this.appService.updateList();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,14 @@ export class AppService {
   getHello(): string {
     return 'Hello World2!';
   }
+
+  deleteList():string{
+    return '刪除清單'
+  }
+  createList():string{
+    return '新增清單'
+  }
+  updateList():string{
+    return '更新清單'
+  }
 }
