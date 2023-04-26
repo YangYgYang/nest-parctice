25f57caa811906c8dc31e917cd1643191f624d78
YangYgYang <ginayoung82@gmail.com>
2023-04-26 23:21:57 +0800

    feat: test(fail)

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -4,6 +4,7 @@ import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,7 +15,24 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('runTest', () => {
+    it('should return an array of users', () => {
+      //預期回傳的資料
+      const result = [
+        { id: 1, username: 'Michael', role: 'user' },
+        { id: 2, username: 'Mary', role: 'user' },
+      ];
+      //jest會監視usersService.()，並給予假資料 result
+      jest.spyOn(service, 'findAll').mockImplementation(() => result);
+
+      controller.findAll(Request, Response, Next).then((data) => {
+        //預期data會跟result一樣
+        expect(data).toBe(result);
+      });
+    });
   });
+});
+
+it('should be defined', () => {
+  expect(controller).toBeDefined();
 });
